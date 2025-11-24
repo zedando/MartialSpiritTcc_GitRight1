96acf15ce3c187ec50958a2688a472bb4a023f66
MaluEspi <maria.espigiorin@gmail.com>
2025-11-24 00:51:04 -0300

    malu

--- TccProject/Assets/Scripts/AnimaçãoComprimento.cs
+++ TccProject/Assets/Scripts/AnimaçãoComprimento.cs
@@ -6,40 +6,32 @@ using UnityEngine.InputSystem;
 public class AnimaçãoComprimento : MonoBehaviour
 {
 
-    public Animator animator;
-    // Start is called before the first frame update
-    void Start()
-    {
-        animator = GetComponent<Animator>();
-    }
+   [Header("Animator do Sensei")]
+    public Animator senseiAnimator;
 
-    // Update is called once per frame
-    void Update()
-    {
+    [Header("Configuração")]
+    public bool destruirDepois = true; // se quiser controlar futuramente
 
-    }
+    private bool jaFez = false; // impede ativar mais de uma vez
+
     private void OnTriggerEnter(Collider other)
     {
+        if (jaFez) return; // já fez uma vez, ignora
+
         if (other.CompareTag("Player"))
         {
-            Comprimento();
+            jaFez = true;
+
+            // Faz o sensei sair do idle (cumprimento)
+            senseiAnimator.SetBool("idle", false);
+
+            // Destrói APENAS o trigger, não o sensei
+            if (destruirDepois)
+                Destroy(gameObject);
         }
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            Idle();
-        }
-    }
-    public void Comprimento()
-    {
-        animator.SetBool("idle", false);
-    }
-    public void Idle()
-    {
-        animator.SetBool("idle", true);
     }
     
     
 }
+
+
